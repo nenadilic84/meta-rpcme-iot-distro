02c4b2344a1aa3ca7d0b5250c997e26b9f842a28
Richard Elberger <rich@richelberger.com>
2021-10-17 18:16:22 -0400

    remove cruft

--- scripts/fleet-provisioning/setup.sh
+++ scripts/fleet-provisioning/setup.sh
@@ -1,20 +1,28 @@
 #+ /bin/sh
 
 if test $# != 1; then
-    echo You need to pass in the role arn for the role having the AWSIoTThingsRegistration managed policy attached.
+    echo You need to pass in role arn for the role having the AWSIoTThingsRegistration managed policy attached.
+    exit 1
 fi
 
-aws iot create-provisioning-template \
+region=${region:-us-east-1}
+profile=${profile:-default}
+std_awscli_args="--output text --region ${region} --profile ${profile}"
+ACCOUNT_ID=$(aws ${std_awscli_args} sts get-caller-identity --output text --query Account)
+
+source_policy=$(dirname $0)/fp-template-claim.json
+target_policy=/tmp/fp-template-claim.$$.json
+
+sed -e "s/ACCOUNT_ID/${ACCOUNT_ID}/g" -e "s/REGION/${region}/g" ${source_policy} > ${target_policy}
+
+aws ${std_awscli_args} iot create-provisioning-template \
     --template-name fp-template-claim \
     --description "Provision by claim with no parameters" \
-    --template-body file://$0/fp-template-claim.json \
+    --template-body file://${target_policy} \
     --enabled \
     --provisioning-role-arn $1
 
-aws iot create-provisioning-claim \
-    --template-name fp-template-claim \
-    > fp-template-claim.out.json
-
-jq .keyPair.PrivateKey out.json | xargs echo -e > fp-template-claim.key.pem
-jq .certificatePem.PrivateKey out.json | xargs echo -e > fp-template-claim.key.pem
-
+if test $? != 0; then
+    echo "creating the provisioning template in the cloud failed.  check errors and try again."
+    exit 1
+fi
